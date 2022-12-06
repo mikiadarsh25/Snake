f44172005cb594d65e46bd5b4e89c588898c1fd4
Adarsh Prakash <57398892+mikiadarsh25@users.noreply.github.com>
2022-12-06 23:01:47 +0530

    Create main.py

---
+++ Email Sender/main.py
@@ -0,0 +1,64 @@
+import smtplib
+from tkinter import *
+
+
+
+def send():
+    my_email = email_box.get()
+    my_password = password_box.get()
+    subject = subject_box.get()
+    message = message_box.get()
+    sender = to_box.get()
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(from_addr=my_email, to_addrs=sender,
+                            msg=f"Subject:{subject}\n\n{message}"
+                            )
+
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=200, height=200)
+logo_image = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_image)
+canvas.grid(row=0, column=1)
+
+email = Label(text="Email:")
+email.grid(row=1, column=0)
+
+email_box = Entry(width=35)
+email_box.grid(row=1, column=1)
+email_box.insert(0, "@gmail.com")
+email_box.focus()
+
+password = Label(text=" App Password:")
+password.grid(row=2, column=0)
+
+password_box = Entry(width=35)
+password_box.grid(row=2, column=1)
+
+to = Label(text="To")
+to.grid(row=3, column=0)
+
+to_box = Entry(width=35)
+to_box.grid(row=3, column=1)
+
+subject = Label(text="Subject:")
+subject.grid(row=4, column=0)
+
+subject_box = Entry(width=35)
+subject_box.grid(row=4, column=1)
+
+message = Label(text="Message:")
+message.grid(row=5, column=0)
+
+message_box = Entry(width=35)
+message_box.grid(row=5, column=1)
+
+send = Button(text="Send", width=36, command=send)
+send.grid(row=6, column=1, columnspan=2)
+window.mainloop()
